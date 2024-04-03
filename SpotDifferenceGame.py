26cf1d269e77c4fd3ba702d6f7216e144b2c33e6
Matt Brundage II <mattjbrundage@icloud.com>
2024-04-02 21:21:37 -0400

    Images & Sounds

---
+++ SpotDifferenceGame.py
@@ -0,0 +1,15 @@
+# Matt Brundage II
+# 4/2/24
+# SpotDifferenceGame.py
+
+import pygame
+from pygame.locals import *
+from time import sleep
+from random import randrange
+
+# initialisation
+pygame.init()
+width = pygame.display.Info().current_w
+height = pygame.display.Info().current_h
+
+# screen
